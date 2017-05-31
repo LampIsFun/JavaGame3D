b619c69d33b6ba3c3c91bc9fce906144f134cd3b
Jesse Jaramillo <jaraj02@mail.sunysuffolk.edu>
2017-05-31 18:06:36 -0400

    Updated method of setting vertices to be more efficient, allowing less resources to be used for larger models.

--- TestingRender/src/mainPack/Main.java
+++ TestingRender/src/mainPack/Main.java
@@ -9,35 +9,33 @@ import renderEngine.Renderer;
 
 public class Main {
 
-	
 	public static void main(String[] args) {
-		
+
 		DisplayManager.createDisplay();
-		
+
 		Loader loader = new Loader();
 		Renderer renderer = new Renderer();
-		
-		float[] vertices = { 
-				//Left bottom triangle
-				-0.5f, 0.4f, 0f, 
+
+		float[] vertices = {
+				-0.5f, 0.5f, 0f, 
 				-0.5f, -0.5f, 0f, 
-				0.5f, -0.1f, 0f, 
-				//Right top triangle
 				0.5f, -0.5f, 0f, 
-				0.5f, 0.5f, 0f, 
-				-0.5f, 0.5f, 0f
+				0.5f, 0.5f, 0f 
+				};
+		int[] indices =  {
+				0,1,3,3,1,2
 		};
-		
-		RawModel model = loader.loadToVAO(vertices);
-		
-		while(!Display.isCloseRequested()) {
+
+		RawModel model = loader.loadToVAO(vertices, indices);
+
+		while (!Display.isCloseRequested()) {
 			renderer.prepare();
-			//game logic
-			
-			//render
+			// game logic
+
+			// render
 			renderer.render(model);
 			DisplayManager.updateDisplay();
-			
+
 		}
 	}
 
