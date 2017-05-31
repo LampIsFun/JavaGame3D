33a16e82daa9c722bf7a406aa0fe499787c27d05
Jesse Jaramillo <jaraj02@mail.sunysuffolk.edu>
2017-05-31 19:57:16 -0400

    Added support for textures.

--- TestingRender/src/mainPack/Main.java
+++ TestingRender/src/mainPack/Main.java
@@ -2,11 +2,13 @@ package mainPack;
 
 import org.lwjgl.opengl.Display;
 
+import models.RawModel;
+import models.TexturedModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
 import renderEngine.Renderer;
 import shaders.StaticShader;
+import textures.ModelTexture;
 
 public class Main {
 
@@ -28,18 +30,20 @@ public class Main {
 				0,1,3,3,1,2
 		};
 		
-		float[] vertices1 = {
-				-0.1f, 0.4f, 0.3f, 
-				-0.1f, -0.3f, 0f, 
-				0.5f, -0.3f, 0f, 
-				0.4f, 0.3f, 0f 
-				};
 		int[] indices1 =  {
 				0,1,3,3,1,2
 		};
+		
+		float[] textureCoords = {
+				0,0,
+				0,1,
+				1,1,
+				1,0
+		};
 
-		RawModel model = loader.loadToVAO(vertices, indices);
-		RawModel model2 = loader.loadToVAO(vertices1, indices1);
+		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("testTexture"));
+		TexturedModel texturedModel = new TexturedModel(model,texture);
 
 		while (!Display.isCloseRequested()) {
 			renderer.prepare();
@@ -47,8 +51,7 @@ public class Main {
 
 			// render
 			shader.start();
-			renderer.render(model);
-			renderer.render(model2);
+			renderer.render(texturedModel);
 			shader.stop();
 			DisplayManager.updateDisplay();
 
