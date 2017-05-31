3c6aceb83b936d1d4d3afd995855b6c604cb32ba
Jesse Jaramillo <jaraj02@mail.sunysuffolk.edu>
2017-05-31 17:38:32 -0400

    Render engine is up and running now, only displays basic models, no shadows yet.

--- TestingRender/src/mainPack/Main.java
+++ TestingRender/src/mainPack/Main.java
@@ -1,12 +1,44 @@
 package mainPack;
 
+import org.lwjgl.opengl.Display;
+
+import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
+
 public class Main {
 
 	
 	public static void main(String[] args) {
-		String x = "This is after print.";
 		
-		System.out.println("Normal System Print. " + x);
+		DisplayManager.createDisplay();
+		
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+		
+		float[] vertices = { 
+				//Left bottom triangle
+				-0.5f, 0.4f, 0f, 
+				-0.5f, -0.5f, 0f, 
+				0.5f, -0.1f, 0f, 
+				//Right top triangle
+				0.5f, -0.5f, 0f, 
+				0.5f, 0.5f, 0f, 
+				-0.5f, 0.5f, 0f
+		};
+		
+		RawModel model = loader.loadToVAO(vertices);
+		
+		while(!Display.isCloseRequested()) {
+			renderer.prepare();
+			//game logic
+			
+			//render
+			renderer.render(model);
+			DisplayManager.updateDisplay();
+			
+		}
 	}
 
 }
