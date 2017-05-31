10a2915101ad316396281ed0af91372969b90688
Jesse Jaramillo <jaraj02@mail.sunysuffolk.edu>
2017-05-31 14:14:16 -0400

    This is a test commit

---
+++ TestingRender/src/mainPack/Main.java
@@ -0,0 +1,12 @@
+package mainPack;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		String x = "This is after print.";
+		
+		System.out.println("Normal System Print. " + x);
+	}
+
+}
