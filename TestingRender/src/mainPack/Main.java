fa9dee4732dcf76301d2261a6ea962828756a2a3
Jesse Jaramillo <jaraj02@mail.sunysuffolk.edu>
2018-02-20 15:06:58 -0500

    School stuff added

--- TestingRender/src/mainPack/Main.java
+++ TestingRender/src/mainPack/Main.java
@@ -1,5 +1,7 @@
 package mainPack;
 
+import java.util.ArrayList;
+
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 
@@ -20,31 +22,35 @@ import textures.TerrainTexturePack;
 public class Main {
 
 	static Loader loader = new Loader();
+	static double newGem;
 
 	public static void main(String[] args) {
 		DisplayManager.createDisplay();
-		
+
+		ArrayList<Entity> gems = new ArrayList<Entity>();
+		Terrain myT;
+
 		//TERRAIN PACK
 		TerrainTexture backgroundTexture = new TerrainTexture(loader.loadTexture("grass"));
-		TerrainTexture rTexture = new TerrainTexture(loader.loadTexture("mud"));
-		TerrainTexture gTexture = new TerrainTexture(loader.loadTexture("grass2"));
+		TerrainTexture rTexture = new TerrainTexture(loader.loadTexture("grass2"));
+		TerrainTexture gTexture = new TerrainTexture(loader.loadTexture("mud"));
 		TerrainTexture bTexture = new TerrainTexture(loader.loadTexture("path"));
-		
+
 		TerrainTexturePack texturePack = new TerrainTexturePack(backgroundTexture,rTexture,gTexture,bTexture);
-		
+
 		TerrainTexture blendMap = new TerrainTexture(loader.loadTexture("blendMap"));
-		
+
 		//
 
 		final int TREE_COUNT = 200;
 		final int GRASS_COUNT = 200;
 		Entity[] tree = new Entity[TREE_COUNT];
 		Entity[] grass = new Entity[GRASS_COUNT];
-		
-		Terrain terrain = new Terrain(-1, -1, loader, texturePack, blendMap, "heightMap");
-		Terrain terrain2 = new Terrain(0, -1, loader, texturePack, blendMap, "heightMap");
-		Terrain terrain3 = new Terrain(-1, 0, loader, texturePack, blendMap, "heightMap");
-		Terrain terrain4 = new Terrain(0, 0, loader, texturePack, blendMap, "heightMap");
+
+		Terrain terrain = new Terrain(-1, -1, loader, texturePack, blendMap, "heightMap2");
+		//Terrain terrain2 = new Terrain(0, -1, loader, texturePack, blendMap, "heightMap2");
+		//Terrain terrain3 = new Terrain(-1, 0, loader, texturePack, blendMap, "heightMap2");
+		//Terrain terrain4 = new Terrain(0, 0, loader, texturePack, blendMap, "heightMap2");
 
 		for (int i = 0; i < TREE_COUNT; i++) {
 			float newX = (float) ((Math.random() * 600) - 300);
@@ -53,48 +59,68 @@ public class Main {
 					new Vector3f(newX, terrain.getY(newX,newY), newY ), 0,
 					(float) ((Math.random() * 360)), 0, 1);
 		}
-		
-		for (int i = 0; i < GRASS_COUNT; i++) {
+
+		/*for (int i = 0; i < GRASS_COUNT; i++) {
 			float newX = (float) ((Math.random() * 600) - 300);
 			float newY = (float) ((Math.random() * 600) - 300);
 			grass[i] = createInstance("grassModel", "grassTexture", true, true, 1f, 0f,
 					new Vector3f(newX, terrain.getY(newX, newY), newY), 0,
 					(float) ((Math.random() * 360)), 0, 4);
-		}
+		}*/
 
 		RawModel modelLight = OBJLoader.loadObjModel("CoolHeadThing", loader);
 		TexturedModel staticModelLight = new TexturedModel(modelLight,
-				new ModelTexture(loader.loadTexture("testTexture")));
+				new ModelTexture(loader.loadTexture("testTexture1")));
 		ModelTexture textureLight = staticModelLight.getTexture();
 
 		textureLight.setShineDamper(10);
 		textureLight.setReflectivity(2);
 
-		Light light = new Light(new Vector3f(3000, 2000, 2000), new Vector3f(1, 1, 1));
+		Light light = new Light(new Vector3f(3000, 9000, 2000), new Vector3f(1, 1, 1));
 
-		
+
 
 		Camera camera = new Camera();
 		camera.setPosition(-2, 5, 10);
 
 		MasterRenderer renderer = new MasterRenderer();
 		while (!Display.isCloseRequested()) {
-
+			newGem = (Math.random()*100);
 			// game logic
+			if (newGem < 2) {
+				float newX = (float) ((Math.random() * 600) - 300);
+				float newY = (float) ((Math.random() * 600) - 300);
+				gems.add(createInstance("diamond","testTexture"+(Math.round(Math.random()*6)+1), false, false, 1f, 0f,
+						new Vector3f(newX, terrain.getY(newX,newY), newY ), 0,
+						(float) ((Math.random() * 360)), 0, 1));
+			}
 				//get terrain currently on.
-			camera.move(terrain);
+			if (camera.getPosition().getX() >= 0 && camera.getPosition().getZ() > 0) {
+				myT = terrain;
+			} else if (camera.getPosition().getX() <= 0 && camera.getPosition().getZ() > 0) {
+				myT = terrain;
+			} else if (camera.getPosition().getX() >= 0 && camera.getPosition().getZ() < 0) {
+				myT = terrain;
+			} else {
+				myT = terrain;
+			}
+			camera.move(myT);
 			// render
 			renderer.processTerrain(terrain);
-			renderer.processTerrain(terrain2);
-			renderer.processTerrain(terrain3);
-			renderer.processTerrain(terrain4);
+			//renderer.processTerrain(terrain2);
+			//renderer.processTerrain(terrain3);
+			//renderer.processTerrain(terrain4);
 
 			for (int i = 0; i < TREE_COUNT; i++) {
 				renderer.processEntity(tree[i]);
 			}
-			for (int i = 0; i < GRASS_COUNT; i++) {
-				renderer.processEntity(grass[i]);
+			for(Entity e:gems) {
+				e.setRotY(e.getRotY()+1f);
+				renderer.processEntity(e);
 			}
+			/*for (int i = 0; i < GRASS_COUNT; i++) {
+				renderer.processEntity(grass[i]);
+			}*/
 
 			renderer.render(light, camera);
 			DisplayManager.updateDisplay();
